aa50f8d61ddd6c983fd9d99dd0332e0c47cc57ad
Pierre Sassoulas <pierre.sassoulas@wisebim.fr>
2018-06-15 18:25:38 +0200

    Feat - Add sanity check for developpers
    It will probably make some life easier.

--- django_zxcvbn_password_validator/zxcvbn_password_validator.py
+++ django_zxcvbn_password_validator/zxcvbn_password_validator.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError, ImproperlyConfigured
 from django.utils.translation import ugettext_lazy as _
 from zxcvbn import zxcvbn
 
@@ -8,11 +8,20 @@ class ZxcvbnPasswordValidator(object):
 
     def __init__(self, min_length=1):
         self.min_length = min_length
+        error_msg = "ZxcvbnPasswordValidator need an integer between 0 and 4 "
+        error_msg += "for PASSWORD_MINIMAL_STRENGH in the settings."
+        try:
+            self.password_minimal_strengh = settings.PASSWORD_MINIMAL_STRENGH
+        except AttributeError:
+            raise ImproperlyConfigured(error_msg)
+        if int(self.password_minimal_strengh) != self.password_minimal_strengh:
+            error_msg += f" (not a {self.password_minimal_strengh.__class__.__name__})"
+            raise ImproperlyConfigured(error_msg)
 
     def validate(self, password, user=None):
         results = zxcvbn(password, user_inputs=user.__dict__)
         password_strengh = results["score"]
-        if password_strengh < settings.PASSWORD_MINIMAL_STRENGH:
+        if password_strengh < self.password_minimal_strengh:
             crack_time = results["crack_times_display"]
             offline_time = crack_time["offline_slow_hashing_1e4_per_second"]
             warn = results["feedback"]["warning"]
@@ -37,7 +46,7 @@ class ZxcvbnPasswordValidator(object):
             4: _("a password that could not be guessed if a large organisation hacked into our database."),
         }
         expectations += " {}".format(hardness.get(
-            settings.PASSWORD_MINIMAL_STRENGH,
+            self.password_minimal_strengh,
             _("our developpers to change the value for 'PASSWORD_MINIMAL_STRENGH' to something valid ASAP, sorry :)")
         ))
         return "{}{}{} {}".format(
