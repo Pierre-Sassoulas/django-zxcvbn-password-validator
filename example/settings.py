69517fdcb44b389f38aded50f45f31e124e23ad6
Pierre Sassoulas <pierre.sassoulas@wisebim.fr>
2018-06-15 19:10:14 +0200

    Fix - Example in french to check i18n and locale included in package

--- example/settings.py
+++ example/settings.py
@@ -105,7 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
+
+LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
 
 TIME_ZONE = 'UTC'
 
