6c35c0b4be34220c62fbf16b982dac0d5fb2d5c9
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2018-07-15 10:25:07 +0200

    Feat - More modular requirements for package and dev
    We don't want to duplicate the requirements and we need a requirements.txt
    for other softwares than setup.py.

--- setup.py
+++ setup.py
@@ -5,12 +5,16 @@ ROOT = os.path.abspath(os.path.dirname(__file__))
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+with open("requirements.txt", "r") as fh:
+    require = fh.readlines()
+require= [x.strip() for x in require]
+
 setup(
     name='django-zxcvbn-password-validator',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['django', 'zxcvbn-python'],
+    install_requires=require,
     license='MIT License',
     description='A password validator for django, based on zxcvbn-python.',
     long_description=open(os.path.join(ROOT, 'README.md')).read(),
