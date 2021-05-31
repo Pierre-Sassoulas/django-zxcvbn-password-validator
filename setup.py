35b847d73182aee66d532435f6dd26f221c5af79
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-05-31 09:08:35 +0200

    Upgrade setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ require = [x.strip() for x in require]
 
 setup(
     name="django-zxcvbn-password-validator",
-    version="1.3.1",
+    version="1.3.2",
     packages=find_packages(),
     include_package_data=True,
     install_requires=require,
