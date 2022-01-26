42b9ef71d3079033b89675c466db160cbf18815b
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-01-26 20:35:33 +0100

    Compatibility with django 4.0 in tests

--- translation_project/urls.py
+++ translation_project/urls.py
@@ -14,7 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url
+
+try:
+    from django.conf.urls import url
+except ImportError:
+    # Django 4.0 replaced url by something else
+    # See https://stackoverflow.com/a/70319607/2519059
+    from django.urls import re_path as url
+
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
