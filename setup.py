2dde281d31d23d91a061980dcc74bc679c7448f7
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-01-26 20:02:48 +0100

    Migrate from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,37 +1,13 @@
-import os
+from pathlib import Path
+from typing import List, Union
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
-ROOT = os.path.abspath(os.path.dirname(__file__))
 
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+def get_requirements(path: Union[str, Path]) -> List[str]:
+    with open(Path(__file__).parent / path, encoding="utf8") as file:
+        content = file.readlines()
+    return content
 
-with open("requirements.txt") as fh:
-    require = fh.readlines()
-require = [x.strip() for x in require]
 
-setup(
-    name="django-zxcvbn-password-validator",
-    version="1.3.2",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=require,
-    license="MIT License",
-    description="A translatable password validator for django, based on zxcvbn-python.",
-    long_description=open(os.path.join(ROOT, "README.md")).read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/Pierre-Sassoulas/django-zxcvbn-password-validator",
-    author="Pierre Sassoulas",
-    author_email="pierre.sassoulas@gmail.com",
-    classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-)
+setup(install_requires=get_requirements("requirements.txt"))
