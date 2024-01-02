9f83d48151f2b82c28b4b7df0f4eaf369b190f01
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2024-01-02 22:52:58 +0100

    Migrate to pyproject.toml, migrate to 'build' from setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-from pathlib import Path
-from typing import List, Union
-
 from setuptools import setup
 
-
-def get_requirements(path: Union[str, Path]) -> List[str]:
-    with open(Path(__file__).parent / path, encoding="utf8") as file:
-        content = file.readlines()
-    return content
-
-
-setup(install_requires=get_requirements("requirements.txt"))
+setup()
